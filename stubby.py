103de0f1e01df54bdcc2a058c0ccb3e0d02ded7d
FidgetYou <fidgetyou@gmail.com>
2017-04-25 21:00:33 -0700

    Stubby should be working now.

--- stubby.py
+++ stubby.py
@@ -1,4 +1,5 @@
 
+
 import sys
 import csv
 from random import randint
@@ -6,28 +7,35 @@ from os.path import expanduser
 #home = expanduser("~")
 
 import csv
+print (sys.argv[1])
+print (sys.argv[2])
 
-with open(sys.argv[2],'r') as csvinput:
-    with open(sys.argv[3], 'w') as csvoutput:
+with open(sys.argv[1],'r') as csvinput:
+    with open(sys.argv[2], "w") as csvoutput:
         writer = csv.writer(csvoutput, lineterminator='\n')
         reader = csv.reader(csvinput)
-
+        csv_list = list(reader)
+        
         all = []
-        row = next(reader)
+        
+        row = csv_list[0]
+        cList = iter(csv_list)
+        #row = csv_list.next()
         row.append("Assigned Team")
         all.append(row)
 
-		#if ((len(csvinput)-1)%3 == 0) :
-		#    rando = randint(1, (len(csvinput)-1)/3)
-		#else:
-		#    rando = randint(1, round((len(csvinput)-1)/3))
-            
-        for row in reader:
-            row.append(randint(1, round((len(csvinput)-1)/3)))
+        #if ((len(csvinput)-1)%3 == 0) :
+        #    rando = randint(1, (len(csvinput)-1)/3)
+        #else:
+        #    rando = randint(1, round((len(csvinput)-1)/3))
+        teamSize = round((len(csv_list)-1)/3)
+        next(cList)
+        for row in cList:
+            row.append(randint(1, teamSize))
             all.append(row)
 
         writer.writerows(all)
-		
-	csvoutput.close()
-	
+        
+    csvoutput.close()
+    
 csvinput.close()
