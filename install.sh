b6745904badda63bfed317ea7b77e51970a4dfa0
Theodore J. LaGrow <tjlagrow@gmail.com>
2017-04-27 14:49:38 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git clone https://github.com/tjlagrow/CIS-422-Group-Project-1.git
+cd CIS-422-Group-Project-1/
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 cd Front_End
